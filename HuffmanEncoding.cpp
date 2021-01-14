aa5f822c3e1acded36ac80194b0b1b39a896d582
ChuAn0428 <77249815+ChuAn0428@users.noreply.github.com>
2021-01-14 01:17:58 -0500

    Add files via upload

---
+++ HuffmanEncoding.cpp
@@ -0,0 +1,19 @@
+#include "PartOne.cpp"
+#include "PartTwo.cpp"
+#include "PartThreeA.cpp"
+#include "PartThreeB.cpp"
+#include "PartFourA.cpp"
+#include "PartFourB.cpp"
+
+
+int main ()
+{
+    partOne_calculateFrequency();
+	partTwo_generateCodeTable();
+	partThreeA_encoding();
+    partThreeB_decoding();
+    partFourA_encoding();
+	partFourB_decoding();
+
+    return 0;
+}
---
+++ PartThreeA.cpp
@@ -0,0 +1,71 @@
+/*
+CSCI 580 Algorithm Design, Analysis, and Implementation
+Program 5 - Part Three A
+Team 1: Ahmet Y. Cengiz, Chu-An Tsai
+*/
+
+#include "utility.h"
+
+void partThreeA_encoding()
+{
+	// Read data from file
+	ifstream readFile;
+	readFile.open("codetable.txt");
+
+	// Give a warning and exit if fail to read the file
+	if (readFile.fail())
+	{
+		cout << "Unable to find codetable.txt!";
+		exit(1);
+	}
+
+	// Import the characters and their frequencies by ASCII index (for encoding use)
+	charCode codeList[128];
+	string first;
+	string second;
+	int n = 0;
+	while (readFile >> first >> second)
+	{
+		if (first == "LF")
+		{
+			codeList[static_cast<int>('\n')].character = first;
+			codeList[static_cast<int>('\n')].code = second;
+		}
+		else if (first == "SP")
+		{
+			codeList[static_cast<int>(' ')].character = first;
+			codeList[static_cast<int>(' ')].code = second;
+		}
+		else
+		{
+			codeList[static_cast<int>(first[0])].character = first;
+			codeList[static_cast<int>(first[0])].code = second;
+		}
+		n++;
+	}
+	readFile.close();
+
+	// Read data from file
+	ifstream readSourseFile("clear.txt", ios::in);
+
+	// Give a warning and exit if fail to read the file
+	if (readSourseFile.fail())
+	{
+		cout << "Unable to find clear.txt!";
+		exit(1);
+	}
+
+	// Prepare to output the codetable
+	ofstream writefile;
+	writefile.open("coded.txt");
+
+	// Output the coded.txt by replacing the character with its code
+	int oneChar;
+	while ((oneChar = readSourseFile.get()) != EOF)
+	{
+		writefile << codeList[oneChar].code;
+		//cout << codeList[oneChar].code << ", ";		// Print out for checking
+	}
+	readSourseFile.close();
+	writefile.close();
+}
